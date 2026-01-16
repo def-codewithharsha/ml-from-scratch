92050bfcd4f587504480d2c86032d3b634692629
unknown <codewithharsha@gmaile.com>
2026-01-16 16:03:33 +0530

    add def fit

--- polynomial_regresssion.py
+++ polynomial_regresssion.py
@@ -9,3 +9,16 @@ class PolynomialRegression:
 
     def _poly_features(self,X):
         return np.hstack([X**i for i in range(self.degree+1)])
+    
+    def fit(self,X,y):
+        X_poly = self._poly_features(X)
+        self.weights = np.zeros(X_poly.shape[1])
+
+        for _ in range(self.epochs):
+            y_pred = X_poly @ self.weights #[X_poly] DOT [self.weight]
+            error= y_pred - y
+
+            gradients = (2 / len(X))* (X_poly.T @ error)
+            self.weights-=self.lr*gradients
+
+    
